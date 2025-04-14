bbe90e88a0cf4e86323ca0196433f4616fd3407b
Fernando-Yonamine <yonamine.fernando@hotmail.com>
2025-04-15 00:34:02 +0900

    adcionado enventos de click, para adicionar novo nome, e outro quando o convidado chegar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inputNome = document.querySelector("#input-nome");
-const listaDeNomes = document.querySelector("#name-list")
+const listaDeNomes = document.querySelector("#name-list");
 const adicionarNome = document.querySelector(".button-item");
-contador = 0
+contador = 0;
 
 
 
@@ -15,16 +15,40 @@ adicionarNome.addEventListener("click", (evento) => {
 
     const nomeDaLista = document.createElement("li");
     const containerNomeDaLista = document.createElement("div");
-    containerNomeDaLista.classList.add("list-item-container")
-    const inputCheckBox = document.createElement("input")
-    inputCheckBox.type = "checkbox"
-    inputCheckBox.id = `checkbox-${contador++}`
-    const nomeConvidado = document.createElement("p")
-    nomeConvidado.innerText = inputNome.value
+    containerNomeDaLista.classList.add("list-item-container");
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.type = "checkbox";
+    inputCheckBox.id = `checkbox-${contador++}`;
+    const nomeConvidado = document.createElement("p");
+    nomeConvidado.innerText = inputNome.value;
+
+    inputCheckBox.addEventListener("click", function(){
+        if(inputCheckBox.checked){
+            nomeConvidado.style.textDecoration = "line-through"
+        }else {
+            nomeConvidado.style.textDecoration = "none"
+        }
+    })
 
     containerNomeDaLista.appendChild(inputCheckBox);
-    containerNomeDaLista.appendChild(nomeConvidado)
+    containerNomeDaLista.appendChild(nomeConvidado);
 
-    nomeDaLista.appendChild(containerNomeDaLista)
-    listaDeNomes.appendChild(nomeDaLista)
+    nomeDaLista.appendChild(containerNomeDaLista);
+    listaDeNomes.appendChild(nomeDaLista);
+
+    const diaSemana = new Date().toLocaleDateString("pt-BR", {weekday: "long"})
+    const data = new Date().toLocaleDateString("pt-BR")
+    const hora = new Date().toLocaleTimeString("pt-BR", {
+        hour:"numeric",
+        minute:"numeric"
+    })
+    
+    const dataCompleta = `${diaSemana} (${data}) às ${hora}`
+    
+    const mostrarData = document.createElement("p")
+    mostrarData.classList.add("texto-data")
+    mostrarData.innerText = dataCompleta
+
+    listaDeNomes.appendChild(mostrarData)
+
 });
